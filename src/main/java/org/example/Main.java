40153ee512a4824ced20e200c00ca325362606ed
smileowsky <gulushovxeyal@gmail.com>
2024-12-26 13:06:01 +0400

    Third Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,23 +14,22 @@ public class Main {
 
         //Create webdriver object and open google.com
         WebDriver driver = new ChromeDriver(optionChrome);
-        driver.get("https://www.google.com/");
+        driver.get("https://demoqa.com/");
 
         //Enter search keyword
-        driver.findElement(By.id("APjFqb")).sendKeys("demoqa");
+        //driver.findElement(By.id("APjFqb")).sendKeys("demoqa");
 
         //Click ENTER button
-        WebElement input = driver.findElement(By.name("q"));
-        input.sendKeys(Keys.RETURN);
-        Thread.sleep(3000);
+        //WebElement input = driver.findElement(By.name("q"));
+        //input.sendKeys(Keys.RETURN);
+        //Thread.sleep(3000);
 
 
-        driver.findElement(By.xpath("/html/body/div[3]/div/div[12]/div/div/div[2]/div[2]/div/div/div[1]/div" +
-                "/div/div/div/div/div/div/div/div[1]/div/span/a/h3")).click();
+        driver.findElement(By.xpath("//html/body/div[2]/div/div/div[2]/div/div[1]/div")).click();
         Thread.sleep(3000);
 
         //Click Elements
-        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div[1]/div/div[1]")).click();
+        //driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/div/div[1]/div/div[1]")).click();
 
         //Click Text Box
         driver.findElement(By.id("item-0")).click();
@@ -66,10 +65,44 @@ public class Main {
         //Click checkbox
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/div[1]/ol/li/span/label/span[1]")).click();
 
-        //
+        //Click Radio Button
         driver.findElement(By.id("item-2")).click();
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/div[2]")).click();
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[2]/div[2]/div[3]")).click();
+
+        //Click Web Tables
+        driver.findElement(By.id("item-3")).click();
+        driver.findElement(By.id("edit-record-1")).click();
+
+        //First Name input
+        WebElement firstName_input = driver.findElement(By.id("firstName"));
+        firstName_input.clear();
+        driver.findElement(By.id("firstName")).sendKeys("Khayal");
+
+        //Last Name input
+        WebElement lastName_input = driver.findElement(By.id("lastName"));
+        lastName_input.clear();
+        driver.findElement(By.id("lastName")).sendKeys("Gulushov");
+
+        //Email input
+        WebElement userEmail_input = driver.findElement(By.id("userEmail"));
+        userEmail_input.clear();
+        driver.findElement(By.id("userEmail")).sendKeys("smileowsky@gmail.com");
+
+        //Age input
+        WebElement age_input = driver.findElement(By.id("age"));
+        age_input.clear();
+        driver.findElement(By.id("age")).sendKeys("30");
+
+        //Salary input
+        WebElement salary_input = driver.findElement(By.id("salary"));
+        salary_input.clear();
+        driver.findElement(By.id("salary")).sendKeys("3500");
+
+        //Department input
+        WebElement department_input = driver.findElement(By.id("department"));
+        department_input.clear();
+        driver.findElement(By.id("department")).sendKeys("Testing");
 
         //Wait
         Thread.sleep(10000);
